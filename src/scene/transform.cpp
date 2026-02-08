ce333a960b205b3fefdb0e34a436020271c8252c
cdrake0924 <chd5548@gmail.com>
2026-02-07 19:13:13 -0500

    wrote initial transform world_local and local_world functions

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -10,17 +10,19 @@ Mat4 Transform::parent_to_local() const {
 }
 
 Mat4 Transform::local_to_world() const {
-	// A1T1: local_to_world
-	//don't use Mat4::inverse() in your code.
-
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if(auto p = parent.lock()){
+		return p->local_to_world() * local_to_parent();
+	} else {
+		return local_to_parent();
+	}
 }
 
 Mat4 Transform::world_to_local() const {
-	// A1T1: world_to_local
-	//don't use Mat4::inverse() in your code.
-
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if(auto p = parent.lock()){
+		return parent_to_local() * p->world_to_local();
+	} else {
+		return parent_to_local();
+	}
 }
 
 bool operator!=(const Transform& a, const Transform& b) {
